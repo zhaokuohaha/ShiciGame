06b51218bd3560c4f12e9de661e8481d9647d86f
zhaokuo <1014336691@qq.com>
2016-09-17 03:28:31 +0800

    基本完成样式

--- src/ShiciGame/wwwroot/areas/relay/relayindex.js
+++ src/ShiciGame/wwwroot/areas/relay/relayindex.js
@@ -1,5 +1,6 @@
 ﻿var websocket;
 $().ready(function () {
+	$('.container').css("min-height", window.innerHeight);
 	websocket = new WebSocket("ws://localhost:12866");
 	websocket.onopen = function () {
 		ShowChatMessage("system-chat", "chat-left chat-success", "连接成功!", "系统");
@@ -26,19 +27,43 @@ $().ready(function () {
 		else {
 			ShowChatMessage("system-chat", "chat-left chat-error", "连接已断开, 请重新连接!", "系统");
 		}
+		$("#textInput").val("");
+	});
+	//回车接龙, ↑键聊天
+	$("#textInput").on('keyup', function (e) {
+		if (e.keyCode != 13 && e.keyCode != 38) return;
+		var msgtype, msgstyle;
+		if (e.keyCode == 13) {//发送诗词验证, 如果验证成功, 发送出去
+			msgstyle = "chat-primary";
+			msgtype = "varse";
+		}else{
+			msgstyle = "chat-default";
+			msgtype = "chat";
+		}
+
+		if (websocket.readyState === websocket.OPEN) {
+			ShowChatMessage("my-chat", "chat-right " + msgstyle, $("#textInput").val(), "我");
+			websocket.send(msgtype + "###" + $("#textInput").val());
+		}
+		else {
+			ShowChatMessage("system-chat", "chat-left chat-error", "连接已断开, 请重新连接!", "系统");
+		}
+		$("#textInput").val("");
 	});
 
 	function ShowChatMessage(role,style, content, username) {
 		var li = $("<li class=\"" + role + "\"></li>");
-		var user = $("<div class=\"user-info unit-0\">\
+		var user = $("<div class=\"user-info\">\
 						<div class=\"user-image\"></div>\
 						<div class=\"user-name\">"+ username + "</div>\
 					</div>");
-		var cont = $("<div class=\"chat-body col " + style + "\">" + content + "</div>");
+		var cont = $("<div class=\"chat-body " + style + "\">" + content + "</div>");
 		if (role === 'my-chat') {
+			li.addClass('flex-right ');
 			cont.addClass('chat-right');
 			li.append(cont).append(user);
 		} else {
+			li.addClass('flex-left ');
 			cont.addClass('chat-left');
 			li.append(user).append(cont);
 		}
